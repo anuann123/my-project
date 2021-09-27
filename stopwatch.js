e957a1a90849f6517d18908dfaa019642ef54929
anaghakr <anaghakr20@gmail.com>
2021-09-27 23:49:04 +0530

    fixed start button

--- stopwatch.js
+++ stopwatch.js
@@ -2,9 +2,13 @@ var min = 0;
 var sec = 0;
 var timer;
 let stopwatch = document.querySelector("p");
+let started = false;
 
 function start() {
-  timer = setInterval(starting, 1000);
+  if (started === false) {
+    timer = setInterval(starting, 1000);
+    started = true;
+  }
 }
 
 function starting() {
@@ -13,19 +17,23 @@ function starting() {
   if (sec === 60) {
     sec = 0;
     min++;
+    stopwatch.innerText = min + ":" + "0" + sec;
   }
   if (min === 60) {
     min = 0;
   }
+
 }
 
 function stop() {
   clearInterval(timer);
+  started = false;
 }
 
 function reset() {
   min = 0;
   sec = 0;
   clearInterval(timer);
+  started = false;
   stopwatch.innerText = (min < 10 ? ("0" + min) : min) + ":" + (sec < 10 ? ("0" + sec) : sec);
 }
